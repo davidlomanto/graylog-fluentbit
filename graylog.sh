a6cd8c9c5e654abd7340e2273c390d63e593b6da
davidlomanto <davidlomanto@yahoo.com.br>
2019-11-28 09:50:14 -0300

    Modificado o arquivo de bash para inicializacao de toda a stack

--- graylog.sh
+++ graylog.sh
@@ -1,16 +1,32 @@
-echo 'tags:
-  install-mongodb: false
-  install-elasticsearch: false
-graylog:
-  mongodb:
-    uri: "mongodb://mongodb:27017/graylog"
-  elasticsearch:
-    hosts: "http://elasticsearch:9200"' > values.yaml
+#! /bin/bash
 
+function podstatus (){
+	result1=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --all-namespaces)
+	while [ ${#result1} -ne 0 ];
+	do
+		result1=$(microk8s.kubectl get pods --field-selector=status.phase!=Succeeded,status.phase!=Running --all-namespaces)
+		sleep 1
+	done
+}
+
+
+cd $(find ~ -maxdepth 2 -iname "graylog-fluentbit")
+
+sudo snap install microk8s --classic
+sudo microk8s.start
+echo "y\n" | sudo microk8s.enable istio
+
+
+echo "aguarde... processando (Istio - 1/3)"
+podstatus
 
 microk8s.kubectl create -f mongo-deploy.yaml
+echo "aguarde... processando (Mongodb - 2/3)"
+podstatus
 
-helm install --namespace "graylog" -n "graylog" stable/graylog --values values.yaml
+echo "aguarde... processando (Graylog - 3/3)"
+
+microk8s.helm install --namespace "graylog" -n "graylog" stable/graylog --values values.yaml
 
 
 
